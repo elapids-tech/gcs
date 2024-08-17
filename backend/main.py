d855fc6390fd287e571c5c7e38aed9e1bcfd4730
“Kento <kentokozuka1045@gmail.com>
2024-08-17 14:49:25 +0900

    update

--- backend/main.py
+++ backend/main.py
@@ -81,7 +81,7 @@ async def websocket_endpoint(websocket: WebSocket):
             
             print('send json data:', data)
             await websocket.send_text(json.dumps(data))
-            await asyncio.sleep(0.016) 
+            await asyncio.sleep(0.05) 
 
     except WebSocketDisconnect:
         manager.disconnect(websocket)
@@ -119,6 +119,59 @@ def stop():
     drone_controller.set_drone_state(0)
     print(f'drone_state:{drone_controller.drone_state}')
     return {"status": f"{drone_controller.drone_state}"}
+
+
+# @app.route("/api/trajectory-planning", methods=["POST"])
+# def trajectory_planning_api():
+#     data = json.loads(request.data)
+
+#     waypoint_groups = [] # grouped by continuious movement (no stopping)
+#     for waypoint in data["waypoints"]:
+#         stop_at_waypoint = waypoint[-1]
+#         if stop_at_waypoint:
+#             waypoint_groups.append([waypoint[:3*num_objects]])
+#         else:
+#             waypoint_groups[-1].append(waypoint[:3*num_objects])
+    
+#     setpoints = []
+#     for i in range(0, len(waypoint_groups)-1):
+#         start_pos = waypoint_groups[i][0]
+#         end_pos = waypoint_groups[i+1][0]
+#         waypoints = waypoint_groups[i][1:]
+#         setpoints += plan_trajectory(start_pos, end_pos, waypoints, data["maxVel"], data["maxAccel"], data["maxJerk"], data["timestep"])
+
+#     return json.dumps({
+#         "setpoints": setpoints
+#     })
+
+# def plan_trajectory(start_pos, end_pos, waypoints, max_vel, max_accel, max_jerk, timestep):
+#     otg = Ruckig(3*num_objects, timestep, len(waypoints))  # DoFs, timestep, number of waypoints
+#     inp = InputParameter(3*num_objects)
+#     out = OutputParameter(3*num_objects, len(waypoints))
+
+#     inp.current_position = start_pos
+#     inp.current_velocity = [0,0,0]*num_objects
+#     inp.current_acceleration = [0,0,0]*num_objects
+
+#     inp.target_position = end_pos
+#     inp.target_velocity = [0,0,0]*num_objects
+#     inp.target_acceleration = [0,0,0]*num_objects
+
+#     inp.intermediate_positions = waypoints
+
+#     inp.max_velocity = max_vel*num_objects
+#     inp.max_acceleration = max_accel*num_objects
+#     inp.max_jerk = max_jerk*num_objects
+
+#     setpoints = []
+#     res = Result.Working
+#     while res == Result.Working:
+#         res = otg.update(inp, out)
+#         setpoints.append(copy.copy(out.new_position))
+#         out.pass_to_input(inp)
+
+#     return setpoints
+
 
 if __name__ == '__main__':
     pass
